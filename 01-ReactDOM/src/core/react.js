4667df06c2e352e540cfa87b2c473188bb8feeba
gyeongsu <choiiisu11@gmail.com>
2024-09-19 17:35:05 +0900

    feat: 01-ReactDOM
    * React 함수형 컴포넌트 개발

---
+++ 01-ReactDOM/src/core/react.js
@@ -0,0 +1,9 @@
+import { _render } from "./internal/root.js";
+
+const React = {
+	createElement: function(type, props, ...children) {
+		return { type, props, children: children.flat() };
+	}
+};
+
+export default React;
