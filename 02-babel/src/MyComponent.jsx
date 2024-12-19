eee966f2a97805358f560a4e2110c2960dba4d54
gyeongsu <choiiisu11@gmail.com>
2024-12-19 13:31:59 +0900

    complete

---
+++ 02-babel/src/MyComponent.jsx
@@ -0,0 +1,28 @@
+/** @jsx h */
+import styled from "./StyledComponents.js";
+
+function h(type, props, ...children) {
+	return { type, props, children: children.flat() };
+}
+
+const Wrapper = styled.div`
+	width: 360px;
+	height: 360px;
+	margin: 0 auto;
+	padding: 10px;
+	border-radius: 30px;
+	background-color: whitesmoke;
+`;
+
+const Title = styled.h1`
+	text-align: center;
+`
+
+function MyComponent() {
+	return (
+		<Wrapper>
+			<Title>제목</Title>
+			<p>내용입니다.</p>
+		</Wrapper>
+	);
+}
---
+++ 02-babel/src/StyledComponents.js
@@ -0,0 +1,16 @@
+const styled = {
+	div: function(styles) {
+		return function(texts, ...elements) {
+			const innerHTML = texts.reduce((result, text, i) => `${result}${text}${elements[i] ? elements[i] : ''}`, '');
+			return `<div style="${styles}">${innerHTML}</div>`;
+		}
+	},
+	h1: function(styles) {
+		return function(texts, ...elements) {
+			const innerHTML = texts.reduce((result, text, i) => `${result}${text}${elements[i] ? elements[i] : ''}`, '');
+			return `<h1 style="${styles}">${innerHTML}</h1>`;
+		}
+	}
+};
+
+export default styled;
